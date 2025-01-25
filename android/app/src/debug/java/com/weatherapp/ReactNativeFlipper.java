5e438c1c2a4cc1d88f834b0d939c2c93e238f288
Tishasaini <06tishasaini@gmail.com>
2025-01-25 22:49:43 +0530

    ReactNativeFlipper.java

--- android/app/src/debug/java/com/weatherapp/ReactNativeFlipper.java
+++ android/app/src/debug/java/com/weatherapp/ReactNativeFlipper.java
@@ -1,9 +1,4 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * <p>This source code is licensed under the MIT license found in the LICENSE file in the root
- * directory of this source tree.
- */
+
 package com.weatherapp;
 
 import android.content.Context;
@@ -46,8 +41,7 @@ public class ReactNativeFlipper {
       client.addPlugin(networkFlipperPlugin);
       client.start();
 
-      // Fresco Plugin needs to ensure that ImagePipelineFactory is initialized
-      // Hence we run if after all native modules have been initialized
+      
       ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
       if (reactContext == null) {
         reactInstanceManager.addReactInstanceEventListener(
